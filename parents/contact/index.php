68b6e6a006635c353efa6b0594194762d5e834f1
Povo <43anillo@gmail.com>
2024-10-08 21:17:07 +0000

    farst commit

---
+++ parents/contact/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!--基本情報-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+        <title>メイン</title>
+        <link rel="icon" href="../favicon.ico">
+        <!--css-->
+        <link rel="stylesheet" href="./index.css">
+    </head>
+	<body>
+        <!--共通-->
+        <!--ヘッダー兼メニュー-->
+        <?php include($_SERVER['DOCUMENT_ROOT']."\parents\common\manu.html"); ?>
+
+            <!--本体-->
+            <div>
+
+            </div>
+        </div>
+        
+    </body>
+</html>
