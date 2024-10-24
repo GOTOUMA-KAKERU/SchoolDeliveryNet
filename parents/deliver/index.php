cd12305dd911d79b149daa7743be24d056c804de
Povo <43anillo@gmail.com>
2024-10-25 07:00:19 +0900

    deliverページ編集

---
+++ parents/deliver/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!--基本情報-->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+        <title>メイン</title>
+        <link rel="icon" href="../favicon.ico">
+        <!--css-->
+        <link rel="stylesheet" href="./index.css">
+    </head>
+	<body>
+        <!--共通-->
+        <!--ヘッダー兼メニュー-->
+        <?php include($_SERVER['DOCUMENT_ROOT']."\parents\common\manu.html"); ?>
+
+            <!--本体-->
+            <div id="main-bg">
+                <div id="main">
+                    <h2>連絡</h2>
+                    <!--リスト-->
+                    <div class="list">
+                        <a href="../../contacts/1.php">
+                            <img src="./images/1.jpeg" alt="">
+                            <h3>AAAA    <span class="date">(2024/10/20)</span></h3>
+                        </a>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
